e297128ec38d0a17ad2b47924b8f0c15594b708f
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-10-27 22:59:56 +0500

    Add files via upload

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+// File: Navbar.jsx located in root-directory/src/component/
+import { Link } from "react-router-dom";
+ 
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <Link to="/" style={{marginRight: "5px"}}>Home</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </div>
+  );
+}
+ 
+export default Navbar;
